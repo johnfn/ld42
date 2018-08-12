b8e2d5dd96dda24bd818471c7ca4e7bbf6032b5b
Bowei Liu <liubowei@gmail.com>
2018-08-11 18:01:30 -0700

    move stuff around

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,3 @@
-class Room extends PIXI.Graphics {
-  constructor(state: State) {
-    super();
-
-    this.beginFill(0x8B572A, 1);
-    this.drawRect(50, 250, 120, 120);
-
-    state.stage.addChild(this);
-  }
-
-  update(state: State): void {
-    state.buttons += 1;
-  }
-}
-
 interface IEntity {
   update: (state: State) => void;
 }
---
+++ src/map.tsx
@@ -0,0 +1,9 @@
+class Map extends PIXI.Graphics {
+  constructor(state: State) {
+    super();
+  }
+
+  update(state: State): void {
+    // state.buttons += 1;
+  }
+}
