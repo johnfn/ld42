6a1f36c38b9d030bdb551a25c79d62f68b8d2b15
Bowei Liu <liubowei@gmail.com>
2018-11-30 22:53:51 -0800

    commit

--- commit.sh
+++ commit.sh
@@ -3,4 +3,6 @@
 git diff | tail -n 30 ; git status ;
 read MESSAGE 
 echo "$MESSAGE"
-git add . && git commit  -m "$MESSAGE"  && git pull -q && git push
+while true ; do
+    git add . && git commit  -m "$MESSAGE"  && git pull -q && git push
+done
